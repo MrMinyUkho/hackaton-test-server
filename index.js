e54167899670cd0cf2b2b183874ce26972a5fa4c
Dmytro <ADimaso007@gmail.com>
2025-02-08 17:28:03 +0200

    Add imports

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
+const mysql      = require('mysql2');
+
+import * as dbs from "dbwork.js";
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port     : '3307',
+  user     : 'root',
+  password : 'password'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.listen(8000, () => {
-  console.log("Example app listening on port 8000!");
+    console.log("Example app listening on port 8000!");
 });
+
+
+connection.end();
